391c1613bded76c6ddfebe28714d3d58c088fc7c
nineclue <nineclue@gmail.com>
2025-02-12 16:45:10 +0900

    basic scalatags invoking rust code

--- src/build.sc
+++ src/build.sc
@@ -6,13 +6,33 @@ object f extends ScalaJSModule {
     def scalaJSVersion = "1.18.2"
     def ivyDeps = Agg(ivy"com.lihaoyi::scalatags::0.13.1")
 
-    def scala  = Task {
-        fastLinkJS()
-        val jsPath = f.fastLinkJS().dest.path
+    /*
+    private def baseScala(t: T[Report]) = Task {
+        val jsPath = t().dest.path
         val targetPath = jsPath / os.up / os.up / os.up
         val target = targetPath / "main.js"
         os.copy.over(jsPath / "main.js", target)
         os.copy.over(jsPath / "main.js.map", targetPath / "smain.js.map")
+        PathRef(target)
+    }
+    def fscala = baseScala(f.fastLinkJS)
+    def scala = baseScala(f.fullLinkJS)
+    */
+
+    def fscala  = Task {
+        val jsPath = f.fastLinkJS().dest.path
+        val targetPath = jsPath / os.up / os.up / os.up
+        val target = targetPath / "main.js"
+        os.copy.over(jsPath / "main.js", target)
+        os.copy.over(jsPath / "main.js.map", targetPath / "main.js.map")
+        PathRef(target)
+    }
+    def scala = Task {
+        val jsPath = f.fullLinkJS().dest.path
+        val targetPath = jsPath / os.up / os.up / os.up
+        val target = targetPath / "main.js"
+        os.copy.over(jsPath / "main.js", target)
+        os.copy.over(jsPath / "main.js.map", targetPath / "main.js.map")
         PathRef(target)
     }
 }
--- src/f/src/Front.scala
+++ src/f/src/Front.scala
@@ -1,3 +1,44 @@
+import scala.scalajs.js, js.annotation.JSGlobal, js.Promise
+import org.scalajs.dom
+import scalatags.JsDom.all.*
+import org.scalajs.dom.Event
+
 object Front:
+    implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+
+    @js.native
+    @JSGlobal("window.__TAURI__.core")
+    object TauriCore extends js.Object:
+        def invoke(fname: String, a: js.Object): Promise[js.Object] = js.native
+
     def main(as: Array[String]): Unit =
         println("Hello from Scala.js")
+        dom.document.body.appendChild(content.render)
+
+    val content = 
+        val nameInput = input(id := "greet-input", placeholder := "Enter a name...").render
+        val greetMsg = p(id := "greet-msg").render
+        def submitHandler(e: Event) = 
+            e.preventDefault()
+            TauriCore.invoke("greet", js.Dynamic.literal("name" -> nameInput.textContent)).toFuture.onComplete: tryo =>
+                tryo.toOption.foreach(jso => greetMsg.textContent = jso.asInstanceOf[String])
+        tag("main")(cls := "container",
+            h1("Welcome to Tauri"),
+            div(cls := "row",
+                a(href := "https://tauri.app", 
+                    target := "_blank",
+                    img(src := "/assets/tauri.svg", cls := "logo tauri", alt := "Tauri logo")),
+                a(href := "https://developer.mozilla.org/en-US/docs/Web/JavaScript", 
+                    target := "_blank",
+                    img(src := "/assets/javascript.svg", cls := "logo vanilla", alt := "JavaScript logo"))
+            ),
+
+            p("Click on the Tauri logo to learn more about the framework"),
+
+            form(cls := "row", id := "greet-form",
+                onsubmit := submitHandler,
+                nameInput, 
+                button(`type` := "submit", "Greet")),
+            greetMsg
+        )
+
